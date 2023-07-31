9b5f9e014b5e4d6239a1022b4198f054c9abebdc
John Smith <a.harmel@esi-sba.dz>
2023-07-31 18:55:54 +0100

    appbar added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -11,6 +11,24 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(40),
+        child: Container(
+          color: const Color.fromRGBO(204, 68, 63, 1),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(width: 60,
+                child: const Placeholder(),),
+                const Icon(Icons.logout,color: Colors.white,
+                size: 28,)
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Center(child:  Text('chatscreen')),
     );
   }
